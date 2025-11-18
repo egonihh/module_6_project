9f4f389d8a8a28d6eb88da92039c7277e467eb7b
Mehmet <mehmetakifbagatir@gmail.com>
2025-11-19 00:52:47 +0100

    modified:   scripts/hash_generator.py

--- scripts/hash_generator.py
+++ scripts/hash_generator.py
@@ -2,4 +2,4 @@
 def generate_sha256_hash():
     import hashlib
     text = input("Enter text to hash: ")
-    print("SHA256:", hashlib.sha256(text.encode()).hexdigest())
+    print("SHA256:", hashlib.sha256(text.encode()).hexdigest()'oldu')
